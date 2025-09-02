f1114d0fcfe250c774be78e1e5cd4c64eb06c5a4
Dieliton <dielitonskt@gmail.com>
2025-09-02 15:36:23 -0300

    FIX: correção de bug exibição de botão fechar ao escanear PPID

--- src/components/ScannerDialog.js
+++ src/components/ScannerDialog.js
@@ -1,74 +1,39 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Html5Qrcode } from 'html5-qrcode';
+import React, { useEffect, useRef } from 'react';
+import { Html5QrcodeScanner } from 'html5-qrcode';
 
 const ScannerDialog = ({ onScanSuccess, onClose }) => {
-    const readerRef = useRef(null);
-    const [isFlashOn, setIsFlashOn] = useState(false);
-    const [isFlashAvailable, setFlashAvailable] = useState(false);
-    const qrCodeScannerRef = useRef(null);
+    const scannerRef = useRef(null);
 
     useEffect(() => {
-        if (!readerRef.current) {
-          return;
-        }
-        // Garante que o elemento 'qr-reader' está vazio antes de instanciar.
-        readerRef.current.innerHTML = "";
-        const qrCodeScanner = new Html5Qrcode(readerRef.current.id);
-        qrCodeScannerRef.current = qrCodeScanner;
+        if (!scannerRef.current) return;
 
-        const successCallback = (decodedText, decodedResult) => {
-            onScanSuccess(decodedText);
-            if (qrCodeScannerRef.current && qrCodeScannerRef.current.isScanning) {
-                qrCodeScannerRef.current.stop();
-            }
-        };
+        const scanner = new Html5QrcodeScanner(
+            scannerRef.current.id,
+            { fps: 10, qrbox: { width: 250, height: 250 } },
+            false // verbose
+        );
 
-        const errorCallback = (errorMessage) => {
-            // ignore
-        };
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
-        Html5Qrcode.getCameras()
-            .then(cameras => {
-                if (cameras && cameras.length) {
-                    const cameraId = cameras[0].id;
-                    qrCodeScanner.start(cameraId, config, successCallback, errorCallback)
-                        .then(() => {
-                            const capabilities = qrCodeScanner.getRunningTrackCameraCapabilities();
-                            if (capabilities.torch) {
-                                setFlashAvailable(true);
-                            }
-                        })
-                        .catch(err => console.error(err));
-                }
-            })
-            .catch(err => {
-                console.error(err);
+        const handleSuccess = (decodedText, decodedResult) => {
+            scanner.clear().then(() => {
+                onScanSuccess(decodedText);
+            }).catch(error => {
+                console.error("Falha ao limpar o scanner.", error);
             });
+        };
 
+        const handleError = (error) => {
+            // console.warn(`QR Code Scan Error: ${error}`);
+        };
+
+        scanner.render(handleSuccess, handleError);
+
+        // Função de limpeza para parar o scanner quando o componente for desmontado
         return () => {
-            if (qrCodeScannerRef.current && qrCodeScannerRef.current.isScanning) {
-                qrCodeScannerRef.current.stop();
-            }
+            scanner.clear().catch(error => {
+                console.error("Falha ao limpar o scanner na desmontagem.", error);
+            });
         };
     }, [onScanSuccess]);
-
-
-    const toggleFlash = () => {
-        if (qrCodeScannerRef.current) {
-            const track = qrCodeScannerRef.current.getRunningTrackCameraCapabilities();
-
-            qrCodeScannerRef.current.applyVideoConstraints({
-                torch: !isFlashOn,
-                advanced: [{torch: !isFlashOn}]
-            }).then(() => {
-                setIsFlashOn(!isFlashOn)
-            }).catch((err) => {
-                console.log(err);
-            });
-        }
-    };
-
 
     return (
         <div className="dialog-overlay" onClick={onClose}>
@@ -78,12 +43,7 @@ const ScannerDialog = ({ onScanSuccess, onClose }) => {
                     <button onClick={onClose} className="close-button">&times;</button>
                 </div>
                 <div className="dialog-body">
-                    <div id="qr-reader" ref={readerRef} />
-                     { isFlashAvailable &&
-                        <button onClick={toggleFlash} className="flash-button">
-                            {isFlashOn ? 'Desligar Flash' : 'Ligar Flash'}
-                        </button>
-                     }
+                    <div id="qr-reader" ref={scannerRef}></div>
                 </div>
             </div>
         </div>
